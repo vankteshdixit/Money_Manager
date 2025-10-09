c03f3bc588d73d5ba718d80d1523e41999ff4164
Vanktesh <dixitvanktesh2003@gmail.com>
2025-10-09 16:39:54 +0530

    ready for production

--- src/main/java/vanktesh/example/Money_Manager/service/ProfileService.java
+++ src/main/java/vanktesh/example/Money_Manager/service/ProfileService.java
@@ -1,5 +1,6 @@
 package vanktesh.example.Money_Manager.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -26,12 +27,15 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("${app.activation.url}")
+    private String activationURL;
+
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile = toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile = profileRepository.save(newProfile);
 //        Send Activation email
-        String activationLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        String activationLink = activationURL +"/api/v1.0/activate?token=" + newProfile.getActivationToken();
         String subject = "Activate your Money Manager account";
         String body = "Click on the following link to activate your account: "+ activationLink;
         emailService.sendEmail(newProfile.getEmail(), subject, body);

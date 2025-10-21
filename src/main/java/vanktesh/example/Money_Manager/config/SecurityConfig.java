ed23d5306c5a036ab71329e8d842e26fa4ef5677
vankteshdixit <dixitvanktesh08@gmail.com>
2025-10-21 16:28:15 +0530

    connected the frontend

--- src/main/java/vanktesh/example/Money_Manager/config/SecurityConfig.java
+++ src/main/java/vanktesh/example/Money_Manager/config/SecurityConfig.java
@@ -34,7 +34,12 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer :: disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/status", "/health", "/register", "/activate", "/login").permitAll()
+                        .requestMatchers(
+                                "/api/v1.0/status",
+                                "/api/v1.0/health",
+                                "/api/v1.0/register",
+                                "/api/v1.0/activate",
+                                "/api/v1.0/login"                ).permitAll()
                         .anyRequest().authenticated())
                         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
